000ef8e58a3d7c460dde36259a21190aa1eb6462
Daquan McDaniel <daquan.mcdaniel@accesscfa.com>
2022-02-28 10:19:00 -0500

    switch theme

--- script.js
+++ script.js
@@ -1,6 +1,7 @@
-let toggleSwitch = document.querySelector('input[type="checkbox]');
+const toggleSwitch = document.querySelector('input[type="checkbox"]');
 
-// Switch Theme Dynamically 
+
+//Switch Theme Dynamically 
 function switchTheme(event) {
     if (event.target.checked) {
       document.documentElement.setAttribute('data-theme', 'dark');
